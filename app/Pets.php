b368adf74c1f9ad91598ba435fe64629a0760899
Rafaelle Bonifacio <rafaelle.bonifacio@gmail.com>
2020-09-21 18:00:50 -0300

    alteração model pets

--- app/Pets.php
+++ app/Pets.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pets extends Model
 {
-  // public $table = "pets";
-  // public $primaryKey = "id";
-  // public $timestamp = true;
-  // public $guarded = [];
+  public $table = "pets";
+  public $primaryKey = "id";
+  public $timestamp = false;
+  public $guarded = [];
 
   // protected $fillable = [
   //   'nome', 'especie', 'genero','nascimento',];
   
-  protected $table = "pets";
+  
 
 
 }
