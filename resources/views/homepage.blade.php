3629f8758eb522c7399485ef7885568f2a55bfa2
jp-caldeira <jpcm86@gmail.com>
2020-10-10 17:04:18 -0300

    comentários na view do produto

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,7 @@
     <!-- Fonte do google pra entrar no codigo -->
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/home.css">            
+    <link rel="stylesheet" href="css/home.css">
 </head>
 
 <body>
@@ -21,13 +21,13 @@
     <div class="tabcontent">
         <img class="img" src="/img/imagem1.png" alt="Gato"></img>
         <p style="width:600px">Produtos que fazem a felicidade do seu pet!</p>
-        
+
         <p style="width:600px">Aqui você encontra uma avaliação justa feita por outros usuários dos produtos para o seu melhor amigo.</p>
         <a  class='btn btn-lg active'  href="{{ route('ranking') }}" >Visite nosso Ranking</a>
     </div>
 </section>
 
-    
+
 
         <section>
             @include('carousel')
@@ -40,13 +40,13 @@
                         <img src="/img/racoes.png" class="rounded float-left" style="width:165px" alt="imagens racao">
                         <img src="/img/camas.png" class="rounded float-left" style="width:165px" alt="imagens camas">
                         <img src="/img/remedios.png" class="rounded float-left" style="width:165px" alt="imagens remedios">
-                        <img src="/img/brinquedos.png" class="rounded float-left" style="width:165px" alt="imagens brinquedos"> 
-                
+                        <img src="/img/brinquedos.png" class="rounded float-left" style="width:165px" alt="imagens brinquedos">
+
                         <button class="botao" style="margin-left:15px">Rações</button>
                         <button class="botao" style="margin-left:40px">Camas</button>
                         <button class="botao" style="margin-left:70px">Remédios</button>
                         <button class="botao" style="margin-left:70px">Brinquedos</button>
-         </div> 
+         </div>
     </section> -->
 
         <section>
