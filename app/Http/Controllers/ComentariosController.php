a896a76a7c230cec0aebcd9dc9b3d8e9e94d2f1d
jp-caldeira <jpcm86@gmail.com>
2020-10-13 22:34:26 -0300

    lista de comentários do usuário

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -60,6 +60,52 @@ class ComentariosController extends Controller
       $comentario->delete();
 
       return redirect()->route('lista-comentarios')->with('mensagem',"Comentário excluído");
-
     }
+
+    public function comentsUser(){
+    $user = auth()->user();
+    $comentarios = $user->comentarios;
+
+    return view('coments-user', ["comentarios" => $comentarios]);
+  }
+
+  public function editComentsUser(Request $request)
+  {
+      $id = $request->id_comment;
+      $comentario = Comentarios::find($id);
+      return view('atualizarComentarioUser', ["comentario" => $comentario]);
+  }
+
+  public function atualizarComentarioUser(Request $request)
+  {
+
+    $mensagemErro = [
+      "required" => "O campo :attribute não pode ser vazio"
+    ];
+
+    $dataValidate = $request->validate([
+      "comentario" => "required",
+      "nota" => "required"
+    ], $mensagemErro);
+
+    $id = $request->id_comment;
+    $comentario = Comentarios::find($id);
+
+    $comentario->body = $request->comentario;
+    $comentario->nota = $request->nota;
+    $comentario->update();
+
+    return view('atualizarComentarioUser', ["comentario" => $comentario, "mensagem" => "Comentário Atualizado"]);
+
+  }
+
+  public function deletarComentarioUser($id)
+  {
+
+    $comentario = Comentarios::find($id);
+    $comentario->delete();
+
+    return redirect()->route('coments-user')->with('mensagem',"Comentário excluído");
+  }
+
 }
